aa583219e45858605615d406f78d6686c26e00a0
MacaulayOsborne <97607543+MacaulayOsborne@users.noreply.github.com>
2023-05-12 02:31:39 +0100

    Contact Form Application

---
+++ greenhubbackend/src/ContactForm.java
@@ -0,0 +1,16 @@
+public class ContactForm {
+           private String name;
+        private String email;
+        private String subject;
+        private String message;
+
+        public ContactForm(String name, String email, String subject, String message) {
+            this.name = name;
+            this.email = email;
+            this.subject = subject;
+            this.message = message;
+        }
+
+        // Getters and setters for the fields
+        // ...
+    }
---
+++ greenhubbackend/src/ContactService.java
@@ -0,0 +1,6 @@
+public class ContactService {
+         public void sendContactForm(ContactForm contactForm) {
+            // Logic to send the contact form data to the customer service department
+            // For example, using an email service or a REST API call
+        }
+    }
